1db1d61ebab117900e60a70342f538e64d503c4c
Kaloan <kaloavalentim8@gmail.com>
2025-04-17 14:02:52 -0300

    adicionada linha com versão da aplicação.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-```csharp
+
 Console.WriteLine(DateTime.Now.ToLongDateString());
+
+Console.WriteLine("versão da aplicação 1.0");
